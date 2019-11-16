98354c81dc1fe0b09b9a423eb2e7d5462a457e78
Michael Petracca <mjpetracca1113@gmail.com>
2019-11-15 22:31:19 -0800

    Added server skeleton.

---
+++ src/server.py
@@ -0,0 +1,59 @@
+import argparse, socket, logging, threading
+
+class ClientThread(threading.Thread):
+
+    def __init__(self, address, socket):
+        threading.Thread.__init__(self)
+        self.csock = socket
+        logging.info('New connection added.')
+
+    
+    def run(self):
+        # exchange messages
+        message = self.recvall(8)
+        msg = message.decode('utf-8')
+        logging.info('Recieved a message from client: ' + msg)
+
+        if msg.startswith('100'):
+            self.csock.sendall(b'200 OK')
+            logging.info('Recieved HELO ok from client.')
+        else:
+            self.csock.sendall(b'500 BAD REQUEST')
+            logging.warning('Bad request from client.')
+        self.csock.close()
+        logging.info('Disconnect client ' + self.role + '.')
+
+    
+    def recvall(self, length):
+        data = b''
+        while len(data) < length:
+            more = self.csock.recv(length - len(data))
+            if not more:
+                logging.error('Did not receive all the expected bytes from server.')
+                break
+            data += more
+        return data
+
+
+def server():
+    # start serving (listening for clients)
+    port = 9001
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind(('localhost',port))
+
+    while True:
+        sock.listen(1)
+        logging.info('Server is listening on port ' + str(port))    
+        
+        # client has connected
+        sc,sockname = sock.accept()
+        logging.info('Accepted connection.')
+        t = ClientThread(sockname, sc)
+        t.start()
+
+
+if __name__ == '__main__':
+    x_taken = False
+    o_taken = False
+    server()
