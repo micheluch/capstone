8fc8103665314ba6306c72f9ac07da85e6306b40
Yevhen-Voitiuk <yevhenvoitiuk@gmail.com>
2019-12-03 23:39:51 -0800

    csv_parser.py: tailored the csv output to cater to SageMaker training (specific parsing quirks). Fixed a couple of bugs with indexes

--- src/csv_parser.py
+++ src/csv_parser.py
@@ -1,30 +1,35 @@
 import csv
 
-data_position = 11 # Pre-determined column of the csv file with data to process
+data_position = 4 # Pre-determined column of the csv file with data to process
 
 # Set the name conventions
 csv_file_path = '/Users/yevhenvoitiuk/Desktop/Capstone/Wireshark Captures/'
-csv_name = 'csv_test.csv'
+csv_name = 'training data.csv'
 
 # Open the output file
 with open(csv_file_path + "PROCESSED " + csv_name, mode="w") as output_csv:
     output_file = csv.writer(output_csv)
 
     # Open the input file
-    with open(csv_file_path + 'csv_test.csv') as input_csv:
+    with open(csv_file_path + csv_name) as input_csv:
         input_file = csv.reader(input_csv)
 
         # Modify the first row to have the custom Attack column
         first_row = next(input_file)
-        output_file.writerow(['Attack?'] + first_row)
-
+        #output_file.writerow(['Attack?'] + first_row) # SageMaker doesn't like header rows
         # Process other rows
         for row in input_file:
             tempRow = row
-            if tempRow[data_position].startswith('3030', 2): # Goal is to change this substring to '3035' for machine training purposes
-                changedData = tempRow[data_position][0:5] + '5' + tempRow[data_position][6:len(tempRow[data_position])]
+            if tempRow[data_position].startswith('5', 5): # Goal is to change this substring to '3030' for machine training purposes
+                changedData = tempRow[data_position][0:5] + '0' + tempRow[data_position][6:len(tempRow[data_position])]
+                print(tempRow[data_position] + "__->__" + changedData)
                 tempRow[data_position] = changedData
+                tempRow[data_position] = int(tempRow[data_position], 16)
                 output_file.writerow([1] + tempRow)
             else:
+                if tempRow[data_position] == '':
+                    tempRow[data_position] = 0
+                else:
+                    tempRow[data_position] = int(tempRow[data_position], 16)
                 output_file.writerow([0] + tempRow)
 
