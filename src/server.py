45380786d6fb259836968367be372cdccbb76e78
micheluch <mjpetracca1113@gmail.com>
2019-12-03 18:30:23 -0800

    Changed from empty string in socket bind to socket.gethostname()

--- src/server.py
+++ src/server.py
@@ -188,7 +188,7 @@ def server():
     port = 9001
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    sock.bind(('' ,port))
+    sock.bind((socket.gethostname(), port))
 
     while True:
         sock.listen(1)
