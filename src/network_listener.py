93839710e9c61c348dfa019bc0699061f01eb7e3
Yevhen Voitiuk <yevhen.voitiuk447@myci.csuci.edu>
2019-12-09 01:11:46 -0800

    network_listener.py: Added an option for two modes of operation: 0 -> The payload data will be unchaged, so attacker codes will come through, 1 -> Payload data will be modified, so normal client and malicious client messages will not be distinguishable,
    Put a check for full packets. If some info is missing, the packet will be thrown away UDP style,
    
    Fixed identations and put comments to clarify some spots,

--- src/network_listener.py
+++ src/network_listener.py
@@ -1,4 +1,4 @@
-import subprocess, shlex, time
+import sys, subprocess, shlex, time
 
 input_command = shlex.split('sudo tcpdump -AUtttvvvXXnns1024 -i lo0 port 9001 -l')
 regex = shlex.split('egrep -o \"00:00:00.[0-9]+|val [0-9]+|ecr [0-9]+|length [0-9][0-9]|3[0-9]30 3\w\w\w \w\w\w\w \w+|\w+.\w+.\w+.\w+.\w+ > \w+.\w+.\w+.\w+.\w+\"')
@@ -7,7 +7,7 @@ regex = shlex.split('egrep -o \"00:00:00.[0-9]+|val [0-9]+|ecr [0-9]+|length [0-
                 #'\'0-9]+|length [0-9][0-9|3[0-9]30 3\w\w\w \w\w\w\w \w+|\w+.\w+.\w+.\w+.\w+ > \w+.\w+.\w+.\w+.\w+\"')
 
 
-def parse_packet(packet_list):
+def parse_packet(packet_list, change_codes):
     delta_time = ''
     val_timestamp = ''
     ecr_timestamp = ''
@@ -16,7 +16,7 @@ def parse_packet(packet_list):
     length = ''
     payload = '0'
     for entry in packet_list:
-        p = subprocess.Popen(regex, stdin=subprocess.PIPE, stdout=subprocess.PIPE, encoding='utf-8', universal_newlines=False, shell=False)
+        p = subprocess.Popen(regex, stdin=subprocess.PIPE, stdout=subprocess.PIPE, encoding='utf-8')
         out = p.communicate(input=entry)[0]
         if out is not None:
             out = out.split('\n')
@@ -44,30 +44,61 @@ def parse_packet(packet_list):
                 temp_length = out[1].split(' ')
                 length = temp_length[1]
 
-            # This block should contain only the payload
+            # This block should contain only the payload, all payloads start with '3'
             elif out[0].startswith('3'):
                 hex_payload = out[0].replace(' ', '')
+                if change_codes == 1:
+                    hex_payload = hex_payload[0:5] + '0' + hex_payload[6:len(hex_payload)]
                 payload = int(hex_payload, 16)
 
         # If no payload block was found, make it 0. Payload block is always the last line
         else:
             payload = '0'
 
+    # If some parts of package are missing, throw it away
+    if length == '' or delta_time == '' or src_port == '' or dest_port == '' or val_timestamp == '' or ecr_timestamp == '':
+        return None
+
     parsed_packet = [length, delta_time, src_port, dest_port, payload, val_timestamp, ecr_timestamp]
     for i in parsed_packet:
         print(i)
+    print("________________")
     return parsed_packet
 
 
-packet = []
-line_count = 0
-p = subprocess.Popen(input_command, stdout=subprocess.PIPE, universal_newlines=False)
-time.sleep(5)
-for row in iter(p.stdout.readline, b''):
-    new_row = row.rstrip().decode('utf-8')
-    packet.append(new_row)
-    line_count += 1
-    if line_count == 6:
-        processed_packet = parse_packet(packet)
-        line_count = 0
-        packet.clear()
+def main():
+    # Main execution
+    if sys.argv[1] == '0':
+        change_codes = 0
+    elif sys.argv[1] == '1':
+        change_codes = 1
+    else:
+        print("Please specify a mode: 0 -> no change; 1 -> change the codes...\n")
+        sys.exit()
+
+    packet = []
+    line_count = 0
+
+    # Execute the tcpdump command, where it listens live on port 9001 and gives out captured packets in real time
+    p = subprocess.Popen(input_command, stdout=subprocess.PIPE, universal_newlines=False)
+    time.sleep(5) # Give some time to initialize
+    for row in iter(p.stdout.readline, b''):
+        new_row = row.rstrip().decode('utf-8')
+        packet.append(new_row)
+        line_count += 1
+        if line_count == 6:
+            processed_packet = parse_packet(packet, change_codes)
+            if processed_packet is not None:
+                print("Placeholder")
+
+
+                # CALL EXTERNAL FUNCTION TO PASS PROCESSED PACKET HERE #    #    #    #
+
+
+            # Reset the line count and packet list, ready for the next packet processing
+            line_count = 0
+            packet.clear()
+
+
+# Run the script
+main()
